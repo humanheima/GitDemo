15ce90bf0c725d2f8d3eff5e8d5c87ed965b6172
humanheima <humanheima@gmail.com>
2018-04-19 11:32:50 +0800

    dev commit

--- app/src/main/java/com/hm/gitdemo/MainActivity.java
+++ app/src/main/java/com/hm/gitdemo/MainActivity.java
@@ -2,9 +2,11 @@ package com.hm.gitdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String TAG=getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void testResetNoConflict() {
+        Log.d(TAG, "testResetNoConflict:i am dev");
 
     }
 }
